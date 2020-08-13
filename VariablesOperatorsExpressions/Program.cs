8bb322356dddf00094fe8661e47930e7c55835e7
Trey Gleason <treygleason8@gmail.com>
2020-08-13 11:39:51 -0700

    added new script

---
+++ VariablesOperatorsExpressions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VariablesOperatorsExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I want to work at the Umbrella Corp.");
+            Console.ReadLine();
+        }
+    }
+}
