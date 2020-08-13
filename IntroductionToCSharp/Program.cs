c060556bf84787120afe07fac3f499c2e88a6639
Trey Gleason <treygleason8@gmail.com>
2020-08-12 17:40:36 -0700

    Add first files

---
+++ IntroductionToCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IntroductionToCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Orange is my favorite color and fruit");
+            Console.ReadLine();
+        }
+    }
+}
+// 1. Create a Git repository
+//    - .gitignore
+// 2. Add files to the Git repository
+// 3. Commit those files to the Git repository
+// 4. Add a remote
+// 5. Push to the remote
